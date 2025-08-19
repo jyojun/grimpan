cd57c2037bfca0d2d5e7a0cca42438c6ee2fde43
jyojun <parkcode98@gmail.com>
2025-08-20 03:05:39 +0900

    feat: 프로토타입 패턴

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -2,10 +2,31 @@ import type Grimpan from "./AbstractGrimpan.js";
 import type IEGrimpan from "./IEGrimpan.js";
 import type ChromeGrimpan from "./ChromeGrimpan.js";
 
+interface Clonable {
+  clone(): Clonable;
+}
+
+class HistoryStack extends Array implements Clonable {
+  clone() {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan;
+  stack: HistoryStack;
+
   protected constructor(grimpan: Grimpan) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
+  }
+
+  getStack() {
+    return this.stack.clone();
+  }
+
+  setStack(stack: HistoryStack) {
+    this.stack = stack.clone();
   }
 
   abstract initialize(): void;
